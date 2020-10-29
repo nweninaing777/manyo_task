d6001a687409167596bebfa6457fa8767845d1e1
nwe ni naing <nweninaing896@gmail.com>
2020-10-29 03:00:53 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-2.times do |n|
+15.times do |n|
     name = Faker::Games::Pokemon.name
     email = Faker::Internet.email
     password = "password"
