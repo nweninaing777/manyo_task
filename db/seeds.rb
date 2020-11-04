3f56e8247129dd80eeaafd21fdd2402ecc6e6020
nwe ni naing <nweninaing896@gmail.com>
2020-11-04 02:08:39 -0500

    edit label create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,28 +23,10 @@
              admin: true)
 
 
-             20.times do |n|
-               name = Faker::Games::Pokemon.name
-               detail = Faker::Games::Pokemon.location
-               deadline = Faker::Date.between(from: Date.tomorrow, to: 7.days.since)
-               status = ["0","1","2"]
-               user_id = rand(1..16)
-               Task.create!(name: name, detail: detail, deadline: deadline, status: rand(0..2), priority: rand(0..2), user_id: user_id)
-             end
 
-
-             Label.create!(
-               name: '開発'
-             )
-
-             Label.create!(
-               name: '設計'
-             )
-
-             Label.create!(
-               name: 'テスト'
-             )
-
-             20.times do |n|
-                 Labeling.create!(task_id: rand(1..20), label_id: rand(1..3))
-             end
+               Label.create([
+                 { name: "Self study" },
+                 { name: "Class" },
+                 { name: "Question" },
+                 { name: "on hold" },
+               ])
